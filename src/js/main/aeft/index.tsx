a57170c88a9a086905a4ce6bb2553736d7dc4e56
Ouvill <contact@ouvill.net>
2023-10-30 20:26:24 +0900

    psdファイルの読み取り

--- src/js/main/aeft/index.tsx
+++ src/js/main/aeft/index.tsx
@@ -1,7 +1,56 @@
+import { Layer, Psd, readPsd } from "ag-psd";
+import { useEffect, useRef, useState } from "react";
+import { evalTS } from "../../lib/utils/bolt";
+
+
+
+
 const Aeft = () => {
+  const psdRef = useRef<HTMLDivElement>(null);
+
+  const [text, setText] = useState<string>("");
+
+  const [path, setPath] = useState<string>("");
+
+  //再帰的に読み込む
+  const printLayerName = (psd: Psd | Layer) => {
+    psd.children?.forEach((child) => {
+      console.log(child.name);
+
+      if (child.children != null) {
+        printLayerName(child);
+      }
+    });
+  };
+
   return (
     <div>
       <h1>After Effects</h1>
+
+      <div ref={psdRef}></div>
+
+      <input
+        type={"file"}
+        onChange={(e) => {
+          if (e.target.files != null && e.target.files.length > 0) {
+            e.target.files[0].arrayBuffer().then((buffer) => {
+              const psd = readPsd(buffer);
+
+              printLayerName(psd);
+
+              if (psdRef.current != null && psd.canvas != null) {
+                psdRef.current.innerHTML = "";
+                psd.canvas.className = "w-40";
+                psdRef.current.appendChild(psd.canvas);
+              }
+            });
+          }
+        }}
+      />
+
+      <div>
+        <div className={"w-40"} ref={psdRef}></div>
+      </div>
     </div>
   );
 };
--- src/jsx/aeft/aeft.ts
+++ src/jsx/aeft/aeft.ts
@@ -5,3 +5,18 @@ export const helloAe = () => {};
 export const appName = () => {
   return BridgeTalk.appName;
 };
+
+export const selectFile = () => {
+  const filterString = Folder.fs === "Windows" ? "All files:*.*" : "";
+
+  const file = File.openDialog(
+    "", //title
+    filterString, // filter available files?
+    false, // allow multiple?
+  );
+
+  if (file) {
+    return file.fsName;
+  }
+  return "";
+};
