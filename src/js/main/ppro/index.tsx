bc6094032dc978a79727b8f44876f502004782c7
Ouvill <contact@ouvill.net>
2023-11-01 20:47:21 +0900

    試行錯誤

--- src/js/main/ppro/index.tsx
+++ src/js/main/ppro/index.tsx
@@ -1,12 +1,42 @@
 import Button from "../../components/button";
+import { csi, evalTS } from "../../lib/utils/bolt";
+import path from "path";
+
+const publicPath = "/public/";
+
+const state_controller_mogrt = "nijikan_state_controller.mogrt";
+
+const getPublicPath = () => {
+  const extensionPath = csi.getSystemPath("extension");
+  return path.join(extensionPath, publicPath);
+};
 
 const Ppro = () => {
   const track = 1;
+  const host = csi.hostEnvironment.appName;
+
+  const importStateControllerMogrt = () => {
+    const mogrtPath = path.join(getPublicPath(), state_controller_mogrt);
+    evalTS("importMogrt", mogrtPath)
+      .then((r) => {})
+      .catch((e) => {
+        alert(e.message);
+      });
+  };
+
+  const moveClip = () => {
+    evalTS("moveClip", 10).catch((e) => {
+      alert(e.message);
+    });
+  };
 
   return (
     <div>
+      <h1>{host}</h1>
       <h1>Premiere Pro</h1>
+
       <div>
+        <p>動かない</p>
         <p>キャラクター設定</p>
         <p>立ち絵ファイル</p>
         <Button>立ち絵読み込み</Button>
@@ -16,8 +46,14 @@ const Ppro = () => {
         <p>表情制御</p>
       </div>
       <div className={"flex flex-col"}>
-        <Button>表情クリップ挿入</Button>
+        <Button onClick={importStateControllerMogrt}>表情クリップ挿入</Button>
+        <Button onClick={moveClip}>jikan</Button>
         <Button>表情クリップを変更/反映</Button>
+      </div>
+      <div>
+        <p>データ送信</p>
+
+        <Button>表情データを反映</Button>
       </div>
     </div>
   );
--- src/jsx/ppro/ppro.ts
+++ src/jsx/ppro/ppro.ts
@@ -1,5 +1,30 @@
+import { addTime, fillMogrtText } from "./ppro-utils";
+
 export const example = () => {};
 
 export const appName = () => {
   return BridgeTalk.appName;
+};
+
+export const importMogrt = (path: string) => {
+  const playerPosition = app.project.activeSequence.getPlayerPosition();
+  const clip = app.project.activeSequence.importMGT(
+    path,
+    playerPosition.ticks,
+    1,
+    0,
+  );
+
+  app.project.activeSequence.setPlayerPosition(clip.end.ticks);
+
+  fillMogrtText(clip, "data", "Hello World");
+};
+
+export const moveClip = (targetSec: number) => {
+  const clip = app.project.activeSequence.getSelection()[0];
+  alert(clip.name);
+  const t = new Time();
+  t.seconds = targetSec;
+
+  clip.start.seconds = 1;
 };
