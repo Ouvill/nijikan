515984b7f1a801509b99425a016c179ca80f3752
Ouvill <contact@ouvill.net>
2023-11-09 22:07:41 +0900

    Refactor findOrCreateBin to use findBin and createBin separately.

--- src/jsx/ppro/scripts/findOrCreateBin.ts
+++ src/jsx/ppro/scripts/findOrCreateBin.ts
@@ -1,10 +1,18 @@
-export const findOrCreateBin = (name: string) => {
+export const findBin = (name: string) => {
   const children = app.project.rootItem.children;
   for (let i = 0; i < children.numItems; i++) {
     const child = children[i];
     if (child.type == 2 /* BIN */ && child.name === name) {
       return child;
     }
+  }
+  return undefined;
+};
+
+export const findOrCreateBin = (name: string) => {
+  const bin = findBin(name);
+  if (bin) {
+    return bin;
   }
   return app.project.rootItem.createBin(name);
 };
