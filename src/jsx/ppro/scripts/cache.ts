fc0f8f32607071b9052dd97476472ab7bc505eee
Ouvill <contact@ouvill.net>
2023-11-09 22:07:01 +0900

    Add cache implementation for storing and retrieving data

---
+++ src/jsx/ppro/scripts/cache.ts
@@ -0,0 +1,26 @@
+type Store = {
+  [key: string]: string;
+};
+
+export type Connection<S extends Record<string, any>> = {
+  get: <K extends keyof S>(key: K) => S[K];
+  set: <K extends keyof S>(key: K, value: S[K]) => void;
+  clear: () => void;
+};
+
+export const initCache = <S extends Record<string, any>>(initialState: S) => {
+  let cache: S = initialState;
+  const connection: Connection<S> = {
+    get: (key) => {
+      return cache[key];
+    },
+    set: (key, value) => {
+      cache[key] = value;
+    },
+    clear: () => {
+      cache = initialState;
+    },
+  };
+
+  return connection;
+};
