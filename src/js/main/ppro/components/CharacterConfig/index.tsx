a3a87cece50569a3928acdc5186911f802070e80
Ouvill <contact@ouvill.net>
2023-11-13 18:20:18 +0900

    Refactor CharacterConfig component by removing unused code and buttons

--- src/js/main/ppro/components/CharacterConfig/index.tsx
+++ src/js/main/ppro/components/CharacterConfig/index.tsx
@@ -1,14 +1,7 @@
-import { evalTS } from "../../../../lib/utils/bolt";
-import path from "path";
 import React from "react";
-import Button from "../../../../components/Button";
-import { getPublicPath } from "../../libs/getPublicPath";
-
 import { Character } from "../../store/settings/characters/type";
 import { SubtitleCharacterConfig } from "./SubtitleCharacterConfig";
 import { LipSyncCharacterConfig } from "./LipSyncCharacterConfig";
-
-const state_controller_mogrt = "nijikan_state_controller.mogrt";
 
 export function CharacterConfig(props: {
   character: Character;
@@ -19,25 +12,6 @@ export function CharacterConfig(props: {
     props.setCharacter({
       ...props.character,
       voiceTrackIndex: value - 1,
-    });
-  };
-
-  const importStateControllerMogrt = () => {
-    const mogrtPath = path.join(getPublicPath(), state_controller_mogrt);
-    evalTS("importMogrt", mogrtPath).catch((e) => {
-      alert(e.message);
-    });
-  };
-
-  const moveClip = () => {
-    evalTS("moveClip", 10).catch((e) => {
-      alert(e.message);
-    });
-  };
-
-  const alertClips = () => {
-    evalTS("alertTracks").catch((e) => {
-      alert(e.message);
     });
   };
 
@@ -79,20 +53,6 @@ export function CharacterConfig(props: {
           character={props.character}
           setCharacter={props.setCharacter}
         />
-      </div>
-
-      <div>
-        <p>表情制御</p>
-      </div>
-      <div className={"flex flex-col"}>
-        <Button onClick={importStateControllerMogrt}>表情クリップ挿入</Button>
-        <Button onClick={moveClip}>moveClip</Button>
-        <Button onClick={alertClips}>alertClips</Button>
-        <Button>表情クリップを変更/反映</Button>
-      </div>
-      <div>
-        <p>データ送信</p>
-        <Button>表情データを反映</Button>
       </div>
     </div>
   );
