82862a31e8b445dc53e89dde3c1b1329e4f5eb27
Ouvill <contact@ouvill.net>
2023-11-15 22:43:09 +0900

    fix button

--- src/js/components/Button.tsx
+++ src/js/components/Button.tsx
@@ -4,17 +4,18 @@ type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
 };
 
-const Button: React.FC<Props> = ({ children, ...props }) => (
+const Button: React.FC<Props> = ({ children, className, ...props }) => (
   <button
-    className={
-      "bg-gray-700 " +
-      "hover:bg-gray-800 " +
-      "active:bg-gray-900  " +
-      "py-2 " +
-      "px-4 " +
-      " " +
-      (props.className || "")
-    }
+    className={[
+      "bg-gray-700 ",
+      "hover:bg-gray-800 ",
+      "active:bg-gray-900  ",
+      "active:border-none",
+      "px-4",
+      "transition",
+      " ",
+      className || "",
+    ].join(" ")}
     {...props}
   >
     {children}
