e270d9f1ebb7977995cde3e5ce5194b63cc85d2d
Ouvill <contact@ouvill.net>
2023-11-18 04:41:22 +0900

    Delete index.test.ts and add migration.test.ts and migration.ts files

--- src/js/main/ppro/store/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { store } from "./index";
-import { expect, test } from "vitest";
-import { defaultState } from "./state";
-
-test("store has expected initial state", () => {
-  const state = store.getState();
-  expect(state).toEqual(defaultState);
-});
