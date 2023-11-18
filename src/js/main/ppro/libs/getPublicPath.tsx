27316d913bd2315bbd5a5d9ab74150af71972494
Ouvill <contact@ouvill.net>
2023-11-19 03:48:42 +0900

    rename function

--- src/js/main/ppro/libs/getPublicPath.tsx
+++
@@ -1,8 +0,0 @@
-import { csi } from "../../../lib/utils/bolt";
-import path from "path";
-
-const publicPath = "/public/";
-export const getPublicPath = () => {
-  const extensionPath = csi.getSystemPath("extension");
-  return path.join(extensionPath, publicPath);
-};
